6c5eb005efb87660aae44636492c23aae2d1c136
Mauricio Rojas <mauriciorojas@MacBook-Pro-de-Mauricio-R.local>
2026-01-19 17:11:12 +0100

    You

--- src/quiz.js
+++ src/quiz.js
@@ -3,13 +3,57 @@ class Quiz {
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
 
+    constructor (questions,timeLimit,timeRemaining){
+
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+
+    }
+
     // 2. getQuestion()
-    
+
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
+
     // 3. moveToNextQuestion()
+
+    moveToNextQuestion() {
+         return this.currentQuestionIndex++
+
+    }
 
     // 4. shuffleQuestions()
 
+    shuffleQuestions() {
+  for (let i = this.questions.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+  }
+}
+
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+  const currentQuestion = this.questions[this.currentQuestionIndex];
+
+  if (answer === currentQuestion.answer) {
+    this.correctAnswers++;
+  }
+}
 
     // 6. hasEnded()
+      hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        }
+        else {
+            return true;
+        }
+      }
+
+
 }
